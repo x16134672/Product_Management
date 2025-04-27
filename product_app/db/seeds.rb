8a779d4df0a3e7b28b8b48925a1ae2af576fbc26
Ilija <x16134672@student.ncirl.ie>
2025-04-28 00:34:26 +0100

    Update db/seeds.rb with fruit products

--- product_app/db/seeds.rb
+++ product_app/db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.create([
+  { name: 'Banana', price: 2.25, available: true, description: 'Big green banana from Africa' },
+  { name: 'Apple', price: 1.89, available: true, description: 'Sweet red apple from southern Spain' },
+  { name: 'Mango', price: 3.75, available: true, description: 'Big sweet mango from Brazil' },
+  { name: 'Tomato', price: 2.45, available: false, description: 'Sweet cherry tomato from Portugal' },
+  { name: 'Red Cherry', price: 4.45, available: true, description: 'Big red sweet-sour cherry from Croatia' },
+  { name: 'Peach', price: 3.01, available: false, description: 'Super sweet peach from Argentina' }
+])
